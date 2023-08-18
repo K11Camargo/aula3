379f62ff5fbc75f55f6d54813e12b9256a1e9a43
K11Camargo <123740343+K11Camargo@users.noreply.github.com>
2023-08-18 17:19:16 -0300

    Update aula03.js

--- aula03.js
+++ aula03.js
@@ -2,6 +2,7 @@
 
 const botaoSomar = document.getElementById('somar')
 const botaoIdentificar = document.getElementById('identificar')
+const botaoNotas = document.getElementById('medias')
 
 function somar (){
     const numero1 = Number (document.getElementById('numero1').value)
@@ -29,6 +30,18 @@ function somar (){
 
     }
 
-
 botaoSomar.addEventListener('click', somar)
 botaoIdentificar.addEventListener('click', identificar)
+
+function notas (){
+    const nota1 = Number (document.getElementById('nota1').value)
+    const nota2 = Number (document.getElementById('nota2').value)
+    const nota3 = Number (document.getElementById('nota3').value)
+    const nota4 = Number (document.getElementById('nota4').value)
+
+    const finaldois = (nota1 + nota2 + nota3 + nota4)
+    media.textContent = finaldois / 4
+    
+}
+
+botaoNotas.addEventListener('click', medias)
